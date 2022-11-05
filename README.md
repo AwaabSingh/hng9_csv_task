64d2a16ac24cee9784903481c4b9176dbe4a68d6
Abdul-Fattah <adeoyin2828@gmail.com>
2022-11-05 23:47:57 +0100

    done

---
+++ README.md
@@ -0,0 +1,15 @@
+# Instructions on using this script
+
+1. Clone the repo to your computer
+
+2. Install the latest version of [Node.js](https://nodejs.org/en/download/).
+
+3. Run "npm install" on the terminal of the cloned repo, this will install the packages needed to use this script.
+
+4. Place your input csv file in the folder of the cloned repo and rename it to data.csv
+
+5. Run "npm start" on the terminal of the cloned repo.
+
+6. A prompt will be shown and ask you to input the required team name e.g bevel, prybar, crankshaft, grit, engine, etc.
+
+7. Click enter and this will generate a "teamName".output.csv and an output.json file
---
+++ app.js
@@ -0,0 +1,66 @@
+const csvToJson = require("csvtojson");
+const fs = require("fs");
+const crypto = require("crypto");
+const csvFilePath = "./csv/data.csv";
+const jsonToCsv = require("csvjson");
+const prompt = require("prompt-sync")({ sigint: true });
+const teamKeys = require("./teamConstants");
+
+let teamName = prompt("Enter Team Name: ").toLowerCase();
+
+csvToJson()
+  .fromFile(csvFilePath)
+  .then((jsonObj) => {
+    const teamRange = teamKeys[teamName];
+    const inputTeamRecords = jsonObj.slice(
+      teamRange.start - 1,
+      teamRange.end - 1
+    );
+
+    try {
+      const hashArray = inputTeamRecords.map((obj) => {
+        let outputJson = { ...obj };
+        let attributes = outputJson["attributes"].split(";");
+        outputJson["attributes"] = attributes.map((attribute) => {
+          if (attribute !== "") {
+            let [trait_type, value] = attribute.split(":");
+            trait_type = trait_type.trim();
+            value = value.trim();
+            return { trait_type, value };
+          }
+        });
+
+        outputJson = {
+          format: "CHIP-0007",
+          name: obj["Name"],
+          description: obj["Description"],
+          minting_tool: teamName,
+          sensitive_content: false,
+          series_number: parseInt(obj["Series Number"]),
+          series_total: jsonObj.length,
+          attributes: obj["attributes"],
+        };
+
+        let hash = crypto
+          .createHash("sha256")
+          .update(JSON.stringify(outputJson))
+          .digest("hex");
+
+        outputJson["Hash"] = hash;
+        outputJson["attributes"] = obj["attributes"];
+        return outputJson;
+      });
+
+      fs.writeFile("output.json", JSON.stringify(hashArray), "utf-8", (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+      const csv = jsonToCsv.toCSV(hashArray, {
+        headers: "key",
+      });
+      fs.writeFileSync(`${teamName}.output.csv`, csv, "utf-8");
+    } catch (error) {
+      console.log("Input team's data has an Invalid format");
+    }
+  });
